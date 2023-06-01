075b83011102dab14472507eb2f899bdee27acc8
Talike <talikeb20@gmail.com>
2023-06-01 19:39:56 -0400

    implemented DELETE requests for posts

--- front_end/src/components/Post.jsx
+++ front_end/src/components/Post.jsx
@@ -1,11 +1,19 @@
 import "./Post.css";
 
 export default function Post({ post }) {
+    async function handleClick(id) {
+        const res = await fetch(`/api/posts/${id}`, { method: "DELETE" });
+
+        if (res.ok) console.log("Post removed!");
+        else console.log("Error removing post.");
+    }
+
     return (
         <div className="post">
             <p className="content">{post.content}</p>
             <p className="author">{post.author}</p>
             <p className="date">{post.createdAt}</p>
+            <img src="/delete_icon.png" alt="delete" onClick={() => handleClick(post._id)} />
         </div>
     );
-}
+} 
