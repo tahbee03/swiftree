a916a4653d9b8638a4a0e5a070d0fad7e39a5744
Talike <talikeb20@gmail.com>
2023-06-23 20:03:14 -0400

    updated design of posts - users can now only delete posts if they belong to them

--- front_end/src/components/Post.jsx
+++ front_end/src/components/Post.jsx
@@ -1,7 +1,7 @@
 import "./Post.css";
 import format from "date-fns/format";
 
-export default function Post({ post }) {
+export default function Post({ post, canDelete }) {
     async function handleClick(id) {
         const res = await fetch(`/api/posts/${id}`, { method: "DELETE" });
 
@@ -10,11 +10,17 @@ export default function Post({ post }) {
     }
 
     return (
-        <div className="post">
-            <p className="content">{post.content}</p>
-            <p className="author">{post.author}</p>
-            <p className="date">{`Posted on ${format(new Date(post.createdAt), "MM/dd/yyyy")}`}</p>
-            <img src="/delete_icon.png" alt="delete" onClick={() => handleClick(post._id)} />
+        <div className="post row">
+            <div className="col-9 info-section">
+                <p className="content">{post.content}</p>
+                <p className="author">{post.author}</p>
+                <p className="date">{`Posted on ${format(new Date(post.createdAt), "MM/dd/yyyy")}`}</p>
+            </div>
+            <div className="col-3 icon-section">
+                {canDelete && (
+                    <img src="/delete_icon.png" alt="delete" onClick={() => handleClick(post._id)} id="delete-icon" />
+                )}
+            </div>
         </div>
     );
 }
