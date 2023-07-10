3b81ed0c03ce5ed7d91bfd3914dd4068003bf559
Talike <talikeb20@gmail.com>
2023-07-10 19:24:19 -0400

    restructured app for deployment

--- back_end/server.js
+++ back_end/server.js
@@ -3,8 +3,11 @@ const express = require("express"); // Include Express.js package
 const posts_api = require("./routes/posts_api"); // Include API routes for posts
 const users_api = require("./routes/users_api"); // Include API routes for users 
 const mongoose = require("mongoose"); // Include Mongoose library
+const cors = require("cors");
 
 const app = express(); // Instantiate Express.js app
+
+// app.use(cors());
 
 app.use(express.json({limit: "100MB"})); // Custom request body size (to process larger files)
 
@@ -22,10 +25,15 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("Connected to database!");
 
-        // Listen for requests (after connecting to database)
-        app.listen(process.env.PORT, () => {
-            console.log(`Listening on port ${process.env.PORT}...`);
-        });
+        if(process.env.PORT) {
+            // Listen for requests (after connecting to database)
+            app.listen(process.env.PORT, () => {
+                console.log(`Listening on port ${process.env.PORT}...`);
+            });
+        }
+
+        // Export app to expose routes to Vercel
+        module.exports = app;
     })
     .catch((err) => {
         console.log(err);
