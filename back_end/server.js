83ed2a22c20394f6ca70e6c9ccdab336b7b8e718
Talike <talikeb20@gmail.com>
2023-07-10 20:51:58 -0400

    ???

--- back_end/server.js
+++ back_end/server.js
@@ -4,10 +4,11 @@ const posts_api = require("./routes/posts_api"); // Include API routes for posts
 const users_api = require("./routes/users_api"); // Include API routes for users 
 const mongoose = require("mongoose"); // Include Mongoose library
 const cors = require("cors");
+const path = require("path");
 
 const app = express(); // Instantiate Express.js app
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json({limit: "100MB"})); // Custom request body size (to process larger files)
 
@@ -19,6 +20,11 @@ app.use("/api/posts", posts_api);
 
 // Routes for user API
 app.use("/api/users", users_api);
+
+// Re-route all other paths to React routes
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "front_end/build/index.html"));
+});
 
 // Connect to database
 mongoose.connect(process.env.MONGO_URI)
