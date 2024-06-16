0cc935252980374e2c5412230b0a1595b0bf31a4
tahbee03 <talikeb20@gmail.com>
2024-06-16 17:35:16 -0400

    basic notification front-end setup

--- front_end/src/pages/Profile.jsx
+++ front_end/src/pages/Profile.jsx
@@ -1,7 +1,8 @@
 import "./Profile.css"; // Styles for Profile page
 
 import PostForm from "../components/PostForm"; // <PostForm />
-import ProfileUpdate from "../components/ProfileUpdate";
+import Notifs from "../components/Notifs"; // <Notifs />
+import ProfileUpdate from "../components/ProfileUpdate"; // <ProfileUpdate />
 import Navbar from "../components/Navbar"; // <Navbar />
 import Footer from "../components/Footer"; // <Footer />
 import PostTree from "../components/PostTree"; // <PostTree />
@@ -24,6 +25,7 @@ export default function Profile() {
     }); // Contains data for the user presented on the page
     const [filteredPosts, setFilteredPosts] = useState([]); // Contains posts to be displayed in the posts container
     const [allPosts, setAllPosts] = useState([]); // Contains all posts related to user
+    const [notifs, setNotifs] = useState([]); // Contains all notifications related to the signed in user
     const [modal, setModal] = useState(null); // Pop-up container to show forms
     const [currentPage, setCurrentPage] = useState(1); // Keeps track of current post tree page
     const [windowWidth, setWindowWidth] = useState(window.innerWidth); // Contains the browser window width
@@ -45,7 +47,7 @@ export default function Profile() {
         return () => window.removeEventListener("resize", () => setWindowWidth(window.innerWidth));
     }, []);
 
-    // Runs when username value is updated
+    // Runs when user and username values are updated
     useEffect(() => {
         const fetchUser = async () => {
             try {
@@ -75,6 +77,15 @@ export default function Profile() {
                 const p = postData.filter((post) => (post.author_id === match._id || post.content.includes(`@${match.username}`)));
                 setAllPosts(p);
                 setFilteredPosts(p);
+
+                if (user && user.username === match.username) {
+                    const notifResponse = await fetch(`${process.env.REACT_APP_API_URL}/notifications`);
+                    const notifData = await notifResponse.json();
+                    if (!notifResponse.ok) throw new Error(notifData.message);
+
+                    const n = notifData.filter((notif) => (notif.user_id = user.id));
+                    setNotifs(n);
+                }
             } catch (error) {
                 setError(handleError(error));
             }
@@ -84,7 +95,7 @@ export default function Profile() {
 
         setError(null);
         fetchUser();
-    }, [username]);
+    }, [user, username]);
 
     // Logs out logged in user
     async function handleLogout() {
@@ -130,6 +141,9 @@ export default function Profile() {
                         {(modal === "post-form") && (
                             <PostForm setModal={setModal} />
                         )}
+                        {(modal === "notifs") && (
+                            <Notifs setModal={setModal} notifications={notifs} />
+                        )}
                         {(modal === "update") && (
                             <ProfileUpdate setModal={setModal} />
                         )}
@@ -141,6 +155,7 @@ export default function Profile() {
                                 {user && (user.username === presentedUser.username) && (
                                     <>
                                         <button type="button" onClick={() => setModal("post-form")}>Make New Post</button>
+                                        <button type="button" onClick={() => setModal("notifs")}>Notifications</button>
                                         <button type="button" onClick={() => setModal("update")}>Settings</button>
                                         <button type="button" onClick={handleLogout}>Log Out</button>
                                     </>
